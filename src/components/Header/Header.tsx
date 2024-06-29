679aa2622e5fe055097d90c621c003ff8ebb9c3a
Ksawery Kuczynski <ksawerykuczynskikk@gmail.com>
2024-06-30 01:18:57 +0200

    Added temporary website icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import NavBar from "./NavBar";
 import { useState } from "react";
 import FloatingBlock from "./FloatingBlock";
 import LoginForm from "./LoginForm";
+import Image from "next/image";
 
 export default function Header() {
     const [showForm, setShowForm] = useState(false);
@@ -16,6 +17,13 @@ export default function Header() {
                 </FloatingBlock>
             : <></>}
             <header className="w-full h-24 bg-gray-900 px-16 flex flex-row content-center items-center">
+                <Image
+                src="/logo.png"
+                width={100}
+                height={100}
+                className="select-none p-4"
+                alt="Website logo"
+                    />
                 <NavBar></NavBar>
                 <button type="button" className="w-28 h-12 bg-amber-500 rounded-lg right-12 absolute" 
                     onClick={() => setShowForm(true)}>
