6f425c84196d5cc93c7feb665e89a6cc6ae5291b
Ksawery Kuczynski <ksawerykuczynskikk@gmail.com>
2024-07-19 20:50:33 +0200

    fix redirect error in production

--- src/libs/getURL.ts
+++ src/libs/getURL.ts
@@ -1,7 +1,8 @@
 const IS_SERVER = typeof window === "undefined";
 export default function getURL(path: string) {
   const baseURL = IS_SERVER
-    ? process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
+  // we predict that website protocol will be always https://
+    ? 'https://'+ process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
     : window.location.origin;
 
   return baseURL + path;
